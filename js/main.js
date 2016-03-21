26c1c58106e1f8cd2935c46a26c7d0f990458f57
Student <Student@Mac-mini-3.local>
2016-03-21 16:28:28 -0600

    progress on portfolio routing

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 angular
   .module('artApp',['ui.router']);
+  .config(MainRouter);
+
+  function MainRouter($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('portfolio', {
+        url:"/portfolio.html",
+        templateUrl: "portfolio.html",
+      });
+  }
