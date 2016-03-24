74c4c34bfdc5a718c6b5336daf0e733b3184b556
Student <Student@Mac-mini-3.local>
2016-03-24 16:49:22 -0600

    fixed js files again and hard-coding photos in

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,64 @@
 (function() {
   angular.module('artControllers',[])
-    .controller('artworkCtrl', artworkController)
+  .controller('artCtrl', artController)
 
+  function artController(){
+    var ac = this
+    ac.artCollection = [
+      {
+        title : 'Stairway to Heaven',
+        image : '../image/blackwhite/stairwaytoheaven.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'Doorception',
+        image : '../image/blackwhite/doorway.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'Wino',
+        image : '../image/blackwhite/wino.jpg',
+        category : 'portrait'
+      },
+      {
+        title : 'meh',
+        image : '../image/blackwhite/_MG_7396.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'meh',
+        image : '../image/blackwhite/_MG_7396.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'meh',
+        image : '../image/blackwhite/_MG_7396.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'meh',
+        image : '../image/blackwhite/_MG_7396.jpg',
+        category : 'travel'
+      },
+      {
+        title : 'meh',
+        image : '../image/blackwhite/_MG_7396.jpg',
+        category : 'travel'
+      },
+    ]
+    // ac.sortArtCollection = sortArtCollection
+    // ac.sortArtCollection()
+    // function sortArtCollection(){
+    // }
+    function artWork(title,image,category){
+      this.artTitle     = title
+      this.artImage     = image
+      this.artCategory  = category
+    }
+
+
+
+
+
+  }
 }())
