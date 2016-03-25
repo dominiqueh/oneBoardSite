b117947667840ad2fafb2c1e7f4db0b640361c7e
Student <Student@Mac-mini-3.local>
2016-03-25 09:10:03 -0600

    possibly progress on filter

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,79 @@
+(function() {
+  'use strict';
 
+  angular.module('photo', [])
+
+  function artCtrl ($scope) {
+    $scope.photo = [
+      {
+        title : 'Stairway to Heaven',
+        image : '../image/blackwhite/stairwaytoheaven.jpg',
+        category : 'travel',
+      },
+      // {
+      //   title : 'Doorception',
+      //   image : '../image/blackwhite/doorway.jpg',
+      //   category : 'travel',
+      // },
+      {
+        title : 'Wino',
+        image : '../image/blackwhite/wino.jpg',
+        category : 'portrait',
+      },
+      // {
+      //   title : 'Lost Boys',
+      //   image : '../image/blackwhite/LostBoys.jpg',
+      //   category : 'travel',
+      // },
+      // {
+      //   title : 'Help',
+      //   image : '../image/blackwhite/help.jpg',
+      //   category : 'portrait',
+      // },
+      // {
+      //   title : 'Lisbon Dogs',
+      //   image : '../image/blackwhite/LisbonDogs.jpg',
+      //   category : 'travel',
+      // },
+      // {
+      //   title : 'Crossed Arms',
+      //   image : '../image/blackwhite/CrossedArms.jpg',
+      //   category : 'portait',
+      // },
+      {
+        title : 'Confused Party',
+        image : '../image/color/confusedparty.jpg',
+        category : 'event',
+      },
+      // {
+      //   title : 'Great Laugh',
+      //   image : '../image/color/laughter.jpg',
+      //   category : 'event',
+      // },
+    ]
+  }
+
+  $scope.photoIncludes = []
+
+  $scope.includeArt = function(photo) {
+    var i = $.inArray(photo, $scope.photoIncludes);
+    if (i > -1) {
+      $scope.photoIncludes.splice(i, 1);
+    } else {
+      $scope.photoIncludes.push(photo);
+    }
+  }
+
+  $scope.photoFilter = function (photo) {
+    if ($scope.photoIncludes.length > 0) {
+      if ($.inArray(photo.category, $scope.photoIncludes) < 0)
+      return;
+    }
+    return photo;
+  }
+}
+
+}());
 
 
 
