b1898f67c9f72ad269b8c0d01973a8812f2b0a62
dominiqueh <dominiquehorner@gmail.com>
2016-03-25 08:34:16 -0600

    last commit of the night

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,8 @@
   angular.module('artControllers',[])
   .controller('artCtrl', artController)
 
+
+
   function artController(){
     // this is now ac to follow scope of artController
     var ac = this
@@ -13,52 +15,53 @@
         image : '../image/blackwhite/stairwaytoheaven.jpg',
         category : 'travel',
       },
-      {
-        title : 'Doorception',
-        image : '../image/blackwhite/doorway.jpg',
-        category : 'travel',
-      },
+      // {
+      //   title : 'Doorception',
+      //   image : '../image/blackwhite/doorway.jpg',
+      //   category : 'travel',
+      // },
       {
         title : 'Wino',
         image : '../image/blackwhite/wino.jpg',
         category : 'portrait',
       },
-      {
-        title : 'Lost Boys',
-        image : '../image/blackwhite/LostBoys.jpg',
-        category : 'travel',
-      },
-      {
-        title : 'Help',
-        image : '../image/blackwhite/help.jpg',
-        category : 'portrait',
-      },
-      {
-        title : 'Lisbon Dogs',
-        image : '../image/blackwhite/LisbonDogs.jpg',
-        category : 'travel',
-      },
-      {
-        title : 'Crossed Arms',
-        image : '../image/blackwhite/CrossedArms.jpg',
-        category : 'portait',
-      },
+      // {
+      //   title : 'Lost Boys',
+      //   image : '../image/blackwhite/LostBoys.jpg',
+      //   category : 'travel',
+      // },
+      // {
+      //   title : 'Help',
+      //   image : '../image/blackwhite/help.jpg',
+      //   category : 'portrait',
+      // },
+      // {
+      //   title : 'Lisbon Dogs',
+      //   image : '../image/blackwhite/LisbonDogs.jpg',
+      //   category : 'travel',
+      // },
+      // {
+      //   title : 'Crossed Arms',
+      //   image : '../image/blackwhite/CrossedArms.jpg',
+      //   category : 'portait',
+      // },
       {
         title : 'Confused Party',
         image : '../image/color/confusedparty.jpg',
         category : 'event',
       },
-      {
-        title : 'Great Laugh',
-        image : '../image/color/laughter.jpg',
-        category : 'event',
-      },
+      // {
+      //   title : 'Great Laugh',
+      //   image : '../image/color/laughter.jpg',
+      //   category : 'event',
+      // },
     ]
 
 
     ac.userPrefs = []
 
     ac.includePref = function(category){
+      console.log($scope.userPrefs)
       var i = $.inArray(category, ac.userPrefs)
       if (i > -1) {
         ac.userPrefs.splice(i,1);
